244e5ab07d70855071f4d086059c08f38a0dd274
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2022-05-19 11:49:13 +0200

    Bump version

--- blender_downloader/__init__.py
+++ blender_downloader/__init__.py
@@ -23,7 +23,7 @@ from tqdm import tqdm
 __author__ = "mondeja"
 __description__ = "Multiplatform Blender portable release downloader script."
 __title__ = "blender-downloader"
-__version__ = "0.0.21"
+__version__ = "0.0.22"
 
 QUIET = False
 
