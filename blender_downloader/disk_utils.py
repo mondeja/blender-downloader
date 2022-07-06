7d5db995a0172c22c854d8aa1c482b0d366bb9a7
gmap <gabehilism@gmail.com>
2022-07-06 04:13:13 -0300

    Fixed a couple of flake8 warnings.

--- blender_downloader/disk_utils.py
+++ blender_downloader/disk_utils.py
@@ -9,19 +9,21 @@ def verify_disk_space(output_directory, total_size_bytes):
             sys.stderr.write(
                     f"Free space: {_get_free_space_at(output_directory)} bytes\n"
                     f"Total size: {total_size_bytes} bytes\n"
-                    f"Not enough space. Exitting.\n"
+                    "Not enough space. Exitting.\n"
                 )
             sys.exit(1)
         sys.stdout.write("OK.\n")
         sys.stdout.write(f"Free space: {_get_free_space_at(output_directory)} bytes\n")
         sys.stdout.write(f"Total size: {total_size_bytes} bytes\n")
-    except FileNotFoundError as e:
-        sys.stderr.write(f"Directory was not found. \n")
+    except FileNotFoundError:
+        sys.stderr.write("Directory was not found. \n")
         sys.exit(1)
 
 def _has_enough_disk_space_at_directory(output_directory, total_size_bytes):
     if total_size_bytes < 0:
-        raise ValueError(f"total_size_bits must be greater than 0. Something went wrong. (got {total_size_bytes}")
+        raise ValueError(
+            f"total_size_bits must be greater than 0. (got {total_size_bytes})"
+        )
 
     free_space_bytes = _get_free_space_at(output_directory)
     return free_space_bytes > total_size_bytes
